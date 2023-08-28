afc8bfa63d70cae23002c0474372619cba41aacc
Gautam Kumar <gk1497singh@gmail.com>
2023-08-28 19:30:31 +0530

    Revert "SN Exp1"
    This reverts commit f5eaf880a52a5e82fe7df5b109e9b66ddd80babc.

--- SN Exp1.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-
-g=nx.Graph()
-g.add_node(1)
-g.add_nodes_from([2,3,4,5,6])
-g.add_edges_from([(1,3),(2,4),(3,4),(4,5),(4,6),(6,5)])
-nx.draw(g,with_labels=1)
-plt.show()
