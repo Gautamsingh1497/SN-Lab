8be2a94d3bf4b2487cfffffaebb38c42da5be5d2
Gautam Kumar <gk1497singh@gmail.com>
2023-09-13 13:16:37 +0530

    SN Exp4

---
+++ SN Exp4.py
@@ -0,0 +1,17 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+G.add_edges_from([('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'D'), ('D', 'E'), ('D', 'F'), ('E', 'F'),('F','G')])
+
+degree_centrality = nx.degree_centrality(G)
+betweenness_centrality = nx.betweenness_centrality(G)
+closeness_centrality = nx.closeness_centrality(G)
+influential_nodes_degree = sorted(degree_centrality, key=degree_centrality.get, reverse=True)[:5]
+influential_nodes_betweenness = sorted(betweenness_centrality, key=betweenness_centrality.get, reverse=True)[:5]
+influential_nodes_closeness = sorted(closeness_centrality, key=closeness_centrality.get, reverse=True)[:5]
+nx.draw(G, with_labels=True)
+print("Influential nodes based on degree centrality:", influential_nodes_degree)
+print("Influential nodes based on betweenness centrality:", influential_nodes_betweenness)
+print("Influential nodes based on closeness centrality:", influential_nodes_closeness)
+plt.show()
